347cbc771f9be7b7ac79b1148a46528b38b576a4
Min Dang <min.dang@butterfly.com.au>
2018-09-07 17:43:09 +1000

    Modify set up file

--- setup.py
+++ setup.py
@@ -1,4 +1,12 @@
-from distutils.core import setup
+import os
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+
+
 
 setup(
     name='django-google-place-auto-complete-widget',
@@ -8,9 +16,7 @@ setup(
     download_url='https://git.butterfly.com.au/django-packages/django-google-place-auto-complete-widget.git',
     license='Apache 2.0',
     author='Min Dang',
-    author_email='dev@luotao.net',
-    maintainer='Lorne Luo',
-    maintainer_email='dev@luotao.net',
-    description='A flexible to push & pull django object to salesforce.',
-    keywords='python django push pull sync salesforce salesforce.com',
+    author_email='min.dang@butterfly.com.au',
+    maintainer='Min',
+    maintainer_email='dmin.dang@butterfly.com.au',
 )
