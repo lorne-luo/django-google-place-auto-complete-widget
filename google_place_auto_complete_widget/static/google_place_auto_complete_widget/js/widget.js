aeeac7a30711b8aa84f55ef4da2fb0f6b70b1890
Min Dang <min.dang@butterfly.com.au>
2018-09-12 13:02:36 +1000

    Update widget.js to turn off autocomplete

--- google_place_auto_complete_widget/static/google_place_auto_complete_widget/js/widget.js
+++ google_place_auto_complete_widget/static/google_place_auto_complete_widget/js/widget.js
@@ -4,6 +4,14 @@ var googlePlaceAutocompleteWidgetInstances = {};
 // Init function which will get called by google api js callback when loaded
 function initGooglePlaceAutocompleteWidget() {
   $(document).ready(function () {
+      
+    // Turn off the filed auto-fill form the browser. 
+    // In Chrome, autocomplete = off does not work. Need to set it to be new-password
+    // However, we cannot simply specify the autocomplete=new-password in HTML since the Google place autocomplete will change that attribute to be off. 
+    $("[data-googleplaceautocompletewidget]").on('focus', function (e) {
+       $(this).attr('autocomplete', "new-password");
+      });
+
     var inputs = document.querySelectorAll('[data-googleplaceautocompletewidget]');
 
     inputs.forEach(function (input) {
